a92b9b3f70ce60f11ed311632dcb97c9981f9753
jilson-asis <jilson.asis7@gmail.com>
2017-12-14 20:53:07 +0800

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,61 @@
+(function() {
+    'use strict';
+
+    window.addEventListener('load', function() {
+        var form = document.getElementById('app-form');
+        form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            } else {
+                submitAppForm();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    }, false);
+})();
+
+function getFormData($form){
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+
+    $.map(unindexed_array, function(n, i){
+        indexed_array[n['name']] = n['value'];
+    });
+
+    return indexed_array;
+}
+
+function submitAppForm() {
+    var formValues = getFormData($('#app-form'));
+
+    axios.post('assets/response.json', formValues)
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+    // verify phone number, using only mocked data
+    // axios.post('https://api.authy.com/protected/json/phones/verification/start', {
+    axios.post('assets/twilio_start.json', {
+        api_key: 'XXXXX',
+        via: 'sms',
+        phone_number: formValues.phone,
+        country_code: '99'
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+    $('#confirmNumber').modal('show');
+}
+
+$('#confirmNumber').click(function(){
+    $(this).modal('hide');
+    $('#congratulations').modal('show');
+});
