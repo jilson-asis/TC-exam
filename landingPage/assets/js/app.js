6c617fb3f8a488fe14080d35652273e807acf61a
jilson-asis <jilson.asis7@gmail.com>
2018-02-18 16:13:50 +0800

    added initial code for the zip validation

--- landingPage/assets/js/app.js
+++ landingPage/assets/js/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    const ZIP_REDIRECT_URL = 'google.com';
+    const ZIP_VERIFY_URL = '../index.html';
+
     var stickyNavTop = $('.page-header').offset().top;
 
     var stickyNav = function(){
@@ -54,5 +57,107 @@ $(document).ready(function() {
         $('html, body').animate({
             scrollTop: top
         }, 500);
+    });
+
+    $('.zip-code-input').keyup(function() {
+        // force int values only
+        $(this).val(isNaN(parseInt($(this).val())) === true ? '' : parseInt($(this).val()));
+
+        $(this).closest('.zip-code-button').removeAttr('data-url');
+
+        var val = $(this).val();
+
+        if (val === '') {
+            return false;
+        }
+
+        var inputChanged = $(this);
+
+        if (val.length === 5) {
+            (new google.maps.Geocoder()).geocode({"address": "USA " + val}, function (results, status) {
+                if (status === google.maps.GeocoderStatus.OK) {
+                    var components = results[0].address_components;
+
+                    var city, state, region, nbhood = "", lat, lng;
+
+                    lat = results[0].geometry.location.lat();
+                    lng = results[0].geometry.location.lng();
+
+                    for (var key in components) {
+                        if (components[key].types[0] === "locality") {
+                            city = components[key].long_name;
+                        }
+
+                        if (components[key].types[0] === "administrative_area_level_1") {
+                            state = components[key].short_name;
+                            region = components[key].long_name;
+                        }
+
+                        if (components[key].types[0] === "neighborhood") {
+                            nbhood = components[key].long_name;
+                        }
+                    }
+
+                    if (!city) {
+                        city = nbhood;
+                    }
+
+                    var data = {
+                        state: state,
+                        address: city + ", " + region,
+                        zip: val,
+                        lat: lat,
+                        lng: lng
+                    };
+
+                    if (city && state && region && lat && lng) {
+                        // saves data to session
+                        $.ajax("/wp-content/themes/jupiter/landing-page/lib/to-session.php", {
+                            type: "POST",
+                            data: data
+                        });
+                    } else {
+                        inputChanged.siblings('.zip-code-message').css("display", "block").addClass("error").html("Please enter valid 5 digits US ZIP code");
+                        inputChanged.addClass('mb-1');
+                        inputChanged.siblings('button').addClass('mt-1');
+
+                        return false;
+                    }
+
+                    // verify on TC API
+                    $.ajax({
+                        method: 'GET',
+                        url: ZIP_VERIFY_URL,
+                        data: data,
+                        success: function (response) {
+                            inputChanged.siblings('.zip-code-message').css("display", "block").addClass("error").html(
+                                "<i class=\"fas fa-check icon-green\"></i> We service " + city + " " + region
+                            );
+
+                            inputChanged.siblings('.zip-code-button').attr('data-url', ZIP_REDIRECT_URL);
+                            inputChanged.addClass('mb-1');
+                            inputChanged.siblings('button').addClass('mt-1');
+                        },
+                        error: function (error) {
+                            inputChanged.siblings('.zip-code-message').css("display", "block").addClass("error").html(
+                                "<i class=\"fas fa-time icon-red\"></i>Sorry, we don't service " + city + " " + region
+                            );
+                            inputChanged.addClass('mb-1');
+                            inputChanged.siblings('button').addClass('mt-1');
+                        }
+                    });
+                } else {
+                    inputChanged.siblings('.zip-code-message').css("display", "block").addClass("error").html("Please enter valid 5 digits US ZIP code");
+                    inputChanged.addClass('mb-1');
+                    inputChanged.siblings('button').addClass('mt-1');
+                }
+            });
+        }
+    });
+
+    $('.zip-code-button').click(function() {
+        if ($(this).attr('data-url') !== '' && $(this).attr('data-url') !== 'undefined' && typeof $(this).attr('data-url') !== 'undefined') {
+            window.location = $(this).attr('data-url');
+        }
     });
 });
